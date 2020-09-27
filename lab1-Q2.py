2d69d80502fc9bf0da7b9c2ac3c0d6b13b88e782
AsakoKagurazaka <tigerzhong1213@gmail.com>
2020-09-28 00:20:33 +1000

    Updated lab1-q2

---
+++ lab1-Q2.py
@@ -0,0 +1,32 @@
+# Copyright 2020 Asako Kagurazaka
+# importing package
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Question2 Histogram
+
+# Read the image
+image1 = cv2.imread("imageQ21.jpg")
+# Using plt.hist to plot histogram (really don't want to write it myself)
+plt.hist(image1.ravel())
+plt.show()  # Display the histogram
+
+# Read imageQ22
+image2 = cv2.imread("imageQ22.tif")
+
+# Split the image into channels
+R, G, B = cv2.split(image2)
+output1_R = cv2.equalizeHist(R)
+output1_G = cv2.equalizeHist(G)
+output1_B = cv2.equalizeHist(B)
+
+# Merge the channels
+image2_t = cv2.merge((output1_R, output1_G, output1_B))
+
+# Equalization completed, Show the difference
+
+cv2.imshow("Original", image2)
+cv2.imshow("Transformed", image2_t)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

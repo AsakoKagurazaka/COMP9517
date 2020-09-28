cd694c01f8af6d29abde07ddded23597db6e1fb3
AsakoKagurazaka <tigerzhong1213@gmail.com>
2020-09-28 19:18:00 +1000

    updated jupyter notebook

--- lab1/lab1-Q5.py
+++ lab1/lab1-Q5.py
@@ -2,8 +2,9 @@
 import cv2
 import numpy as np
 
-# Question 1 Contrast Stretching
+# Question 5 Restore image
 
+# Using OpenCV functions as per https://piazza.com/class/kemg8hc3toq3td?cid=79 (The lecturer said so)
 image1 = cv2.imread("imageQ5.png", 0)  # Read image
 cv2.imshow("Original Image", image1)
 cv2.waitKey(0)
@@ -29,15 +30,23 @@ cv2.imshow("Increased Contrast", cont)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# Perform histogram equalization
-hist_eq = cv2.equalizeHist(cont)
+# Perform a High Pass Filter
+lpf = cv2.GaussianBlur(cont, (5, 5),0)
+hpf = cv2.subtract(cont, lpf)
+hpfed = cv2.add(cont,hpf)
 
+cv2.imshow("High Pass Filter", hpfed)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Perform histogram equalization
+hist_eq = cv2.equalizeHist(hpfed)
 cv2.imshow("Histogram Equalization", hist_eq)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Show the result
-result = hist_eq  
+result = hist_eq
 cv2.imshow("Restored Image", result)
 
 # Save the result to file. The code is ran once before submitting.

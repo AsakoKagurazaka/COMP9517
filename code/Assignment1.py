75df7b1f3a436428618b4ee1e1b7bd6f5f4fe4fb
AsakoKagurazaka <tigerzhong1213@gmail.com>
2020-09-29 10:43:23 +1000

    added ipynb

--- code/Assignment1.py
+++ code/Assignment1.py
@@ -4,7 +4,7 @@ Created on Tue Sep 15 23:05:46 2020
 
 @author: AsakoKagurazaka
 """
-#......IMPORT .........
+import cv2
 import argparse
 def task1():
     print("draft code")
@@ -15,13 +15,9 @@ def task2():
 def task3():
     print("draft code")
 
-""" Commented at this stage for debugging
 my_parser = argparse.ArgumentParser()
 my_parser.add_argument('-o','--OP_folder', type=str,help='Output folder name', default = 'OUTPUT')
 my_parser.add_argument('-m','--min_area', type=int,action='store', required = True, help='Minimum pixel area to be occupied, to be considered a whole rice kernel')
 my_parser.add_argument('-f','--input_filename', type=str,action='store', required = True, help='Filename of image ')
 # Execute parse_args()
 args = my_parser.parse_args()
-"""
-import cv2
-epsilon = 0.01
